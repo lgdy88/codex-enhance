d001ba0c4be3c4d020c0b91bc5bd9d33edd65cdf
lideyan <lideyan@itcast.cn>
2026-05-25 21:57:56 +0800

    feat: 更新 Dex 图标资产

--- assets/inject/renderer-inject.js
+++ assets/inject/renderer-inject.js
@@ -37,7 +37,7 @@
   const codexArchiveRowActionsVersion = "1";
   const codexArchiveDeleteAllVersion = "2";
   const codexConversationTimelineVersion = "2";
-  const codexPlusVersion = "1.0.15";
+  const codexPlusVersion = "1.0.16";
   const codexPlusDisplayName = "Dex";
   const codexPlusSettingsKey = "codexPlusSettings";
   window.__codexProjectMoveRuntimeId = (window.__codexProjectMoveRuntimeId || 0) + 1;
